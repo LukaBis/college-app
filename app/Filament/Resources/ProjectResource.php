9f9d63d65b74b8ef7bcb51234cfd0c8690941e98
Luka <LukaBis@lbiskupic05@gmail.com>
2024-06-24 01:02:45 +0200

    naviagtion clean up

--- app/Filament/Resources/ProjectResource.php
+++ app/Filament/Resources/ProjectResource.php
@@ -18,6 +18,7 @@ class ProjectResource extends Resource
     protected static ?string $model = Project::class;
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static bool $shouldRegisterNavigation = false;
 
     public static function form(Form $form): Form
     {
